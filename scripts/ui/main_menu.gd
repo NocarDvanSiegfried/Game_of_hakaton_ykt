e36b1a3a528427a4db42a1749639a460d7e656e0
Ivan <novgorodov.2011@mail.ru>
2026-06-01 12:01:04 +0900

    добавил звук в меню

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -2,8 +2,12 @@ extends Control
 
 const GAME_SCENE := "res://scenes/main.tscn"
 
+@onready var menu_music: AudioStreamPlayer = $MenuMusic
+
 
 func _ready() -> void:
+	if not menu_music.playing:
+		menu_music.play(0.0)
 	%NewGameButton.pressed.connect(_on_new_game_pressed)
 	%QuitButton.pressed.connect(_on_quit_pressed)
 
